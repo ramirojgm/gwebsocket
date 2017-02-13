e1e1f55352e921428789f77cb2164a00656232d0
ramirojgm <ramiro.jgmp@gmail.com>
2017-02-13 00:27:22 -0600

    UPDATE LICENSE

--- gwebsocket/httppackage.h
+++ gwebsocket/httppackage.h
@@ -1,5 +1,22 @@
-#ifndef _HTTP_PACKAGE_
-#define _HTTP_PACKAGE_
+/*
+	Copyright (C) 2017 Ramiro Jose Garcia Moraga
+
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HTTP_PACKAGE_
+#define HTTP_PACKAGE_
 
 #include <glib.h>
 #include <gio/gio.h>
